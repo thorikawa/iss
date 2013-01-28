0b8bfa42d7e2667266a80c813a34b3389cd7c48b
Takahiro Horikawa <horikawa.takahiro@gmail.com>
2013-01-28 11:23:53 -0800

    Add cps solver

---
+++ src/iss/CPSSolver.java
@@ -0,0 +1,25 @@
+package iss;
+
+public class CPSSolver {
+    public static final double sarjMaxVelocity = 0.15;
+
+    public static final double sarjMaxAcceleration = 0.005;
+
+    public static double[] solveSarj(double[] rotations, double initial) {
+
+        int prevIndex = 0;
+        Range prevVelocityRange;
+        // forward check
+
+        // backword check
+
+        return null;
+    }
+
+    public class Range {
+        public double min;
+
+        public double max;
+    }
+
+}
